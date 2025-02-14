d85132e686a90afbaee362f5099a75bdf7821c72
『AnTiOdErHuBdEvMaYbE』 <zhulinevgenij0@gmail.com>
2025-02-14 18:17:21 +0700

    Update AntiOderHub3.30.lua

--- AntiOderHub3.30.lua
+++ AntiOderHub3.30.lua
@@ -1,5 +1,5 @@
 -- Script was removed from old account and moved to secret account
 -- Also I made new obfuscate technology and you can't steal my script more :D
 
-loadstring(game:HttpGet("https://antioderhub.github.io/ScriptSourceCode/"))()Ioad("72f100d66a3bcbc55fddde2cf51a98f8")
+loadstring(game:HttpGet("https://antioderhub.fun/ScriptSourceCode/"))()Ioad("72f100d66a3bcbc55fddde2cf51a98f8")
 
